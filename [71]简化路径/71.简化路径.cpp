9252d8f4b2c13e83bcae8ac4f77931865de66b52
Winszhenhg <2302939621@qq.com>
2021-05-02 16:44:51 +0800

    test

---
+++ [71]简化路径/71.简化路径.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=71 lang=cpp
+ *
+ * [71] 简化路径
+ */
+
+// @lc code=start
+// 没思路，看题解吧
+class Solution {
+public:
+    string simplifyPath(string path) {
+
+    }
+};
+// @lc code=end
+
